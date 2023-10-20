7bf456cec8a52c767368ed4f0bbaedb26ad4738a
wslyvh <wslyvh@users.noreply.github.com>
2023-10-20 10:06:45 +0200

    build

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@ const withPWA = require('@ducanh2912/next-pwa').default({
   dest: 'public',
 })
 
-module.exports = withPWA({
-  // Your Next.js config
-})
+/** @type {import("next").NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+module.exports = withPWA(nextConfig)
