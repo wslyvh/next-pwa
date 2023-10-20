70efa1633459a94c8385992e93345b66870faf6b
wslyvh <wslyvh@users.noreply.github.com>
2023-10-20 09:36:41 +0200

    next-pwa

---
+++ src/app/layout.tsx
@@ -0,0 +1,59 @@
+import type { Metadata } from 'next'
+import { PropsWithChildren } from 'react'
+import '@/assets/globals.css'
+import { PWALifeCycle } from '@/components/lifecycle';
+
+const APP_NAME = "PWA App";
+const APP_DEFAULT_TITLE = "My Awesome PWA App";
+const APP_TITLE_TEMPLATE = "%s - PWA App";
+const APP_DESCRIPTION = "Best PWA app in the world!";
+
+export const metadata: Metadata = {
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  manifest: "/manifest.json",
+  themeColor: "#FFFFFF",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+}
+
+export default function RootLayout(props: PropsWithChildren) {
+  return (
+    <html lang="en">
+      <body>
+        <>
+          <PWALifeCycle />
+          {props.children}
+        </>
+      </body>
+    </html>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen">
+      <div className="flex flex-col items-center justify-center w-full">
+        <h1 className="text-6xl font-bold">Hello, world!</h1>
+        <p className="mt-4 text-2xl">Welcome to your new Next.js app.</p>
+      </div>
+    </main>
+  )
+}
